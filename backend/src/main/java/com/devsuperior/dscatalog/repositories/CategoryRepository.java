ec633af19d4584ef161c27928eb066b7b2586027
Bia Coelho <biacoelho@pm.me>
2020-10-02 16:55:40 -0300

    Find category by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscatalog.entities.Category;
 
-/**
- * os objetos do tipo CategoryRepository passa a ser gerenciados
- * pelo spring
+/** Classe resposável por acessar o banco de dados
+ * os objetos do tipo CategoryRepository passa a
+ * ser gerenciados pelo spring
  * @author Bia Coelho
  */
 
--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,8 @@ import com.devsuperior.dscatalog.services.CategoryService;
 
 
 /**
- * This class implements the RestController. The resources.
+ * This class implements the RestController. The resources e pelo
+ * o que eu entendi cada rota é chamada de endpoint
  * @author https://github.com/biacoelho
  */
 
@@ -29,5 +31,11 @@ public class CategoryResource {
 		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value= "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO dto = service.finById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
