1b396a38be238aa192a89100d18f7fc0e75bd8f9
Bia Coelho <biacoelho@pm.me>
2020-12-15 12:38:17 -0300

    Finish catalog loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { ProductsResponse } from '../../core/types/Product';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 
 import './styles.scss';
 
@@ -13,6 +14,7 @@ const Catalog = () => {
   */
 
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const params = {
@@ -20,20 +22,28 @@ const Catalog = () => {
       linesPerPage: 12,
     };
 
+    // iniciar o loader pra mostrar na tela
+    setIsLoading(true);
     makeRequest({ url: '/products', params }).then((response) =>
-      setProductsResponse(response.data),
-    );
+      setProductsResponse(response.data))
+      .finally(() => {
+        // finalizar o loader
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <div className="catalog-container">
       <h1 className="catalog-title">Catálogo de produtos</h1>
       <div className="catalog-products">
-        {productsResponse?.content.map((product) => (
-          <Link to={`/products/${product.id}`} key={product.id}>
-            <ProductCard product={product}/>
-          </Link>
-        ))}
+        {isLoading ? <ProductCardLoader /> : (
+          productsResponse?.content.map((product) => (
+            <Link to={`/products/${product.id}`} key={product.id}>
+              <ProductCard product={product}/>
+            </Link>
+          ))
+        )}
+        
       </div>
     </div>
   );
